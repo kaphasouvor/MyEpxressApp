3de9fd212befc6bae0b15dc4d291833cf8c81785
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-05-20 20:52:10 -0400

    adding server.js

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const keys = require('./keys');
+const User = require('./models/User');
 const app = express()
 const port = 3000
 
+// Connecting to mongoDB
+const mongoose = require('mongoose');
+mongoose.connect (keys.mongoDBUrl, { useNewUrlParser: true })
+.then(() => console.log("DB connected"));
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', function (req, res){
     res.send('Hello World!')
 })
 
-app.get('/api', function(req, res){
-    const userName = req.query.username;
-    const userId = req.query.id;
-    const message = req.query.message;
-    console.log(userName);
-    const reply = `${userName} with ID# ${userId} is saying ${message}`;
-    res.send(reply);
+app.post('/api', function(req, res){
+    const userName = req.body.username;
+    const message = req.body.message;
+   
+    const data = {
+        username: userName,
+        message: message
+    }
+    console.log(data);
+
+    const user = new User(data)
+    user.save().then(() => {
+            console.log("New user created");
+            res.send(data);
+    })
 })
 
 app.get('/showprofile/:username', function(req, res){
-    const user = req.query.user;
-    console.log(user);
+    const user = req.params.username;
     res.send("show profile working");
+    console.log(user);
 })
 
+app.get("/getallusers", function (req, res) {
+    res.send("data");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
